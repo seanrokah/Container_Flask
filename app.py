8646e40534c360f2cdd96444022a7af305bda247
Sean Rokah <seanrokah@gmail.com>
2025-04-25 10:19:49 +0300

    Complete Flask container setup

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python3
+########################################################
+# Simple Flask App
+# Name : Sean Rokah
+# Date : 2025-04-25
+# Description : A simple Flask app to display a welcome message
+# Version : 1.0
+########################################################
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
